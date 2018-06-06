914fab488e528e577f9412d95ec0bfee3e76b4c6
Wojciech Frącz <fraczwojciech@gmail.com>
2018-06-06 12:29:17 +0200

    Should work on heroku!

---
+++ src/main/frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>System do zapisów na zajęcia</h1>
+    <div v-if="!auth">
+      <login-form @login="enter($event)"></login-form>
+    </div>
+    <div v-show="auth">
+      <h2>Witaj {{ auth }}!
+      <a @click="logout()" class="float-right  button-outline button">Wyloguj</a>
+      </h2>
+
+      <meetings-page :username="auth"></meetings-page>
+    </div>
+  </div>
+</template>
+
+<script>
+import "milligram";
+import LoginForm from "./LoginForm";
+import MeetingsPage from "./meetings/MeetingsPage";
+
+export default {
+  components: {LoginForm, MeetingsPage},
+  data() {
+    return {
+      auth: ""
+    };
+  },
+  methods: {
+    enter(user) {
+      this.auth = user;
+    },
+    logout() {
+      this.auth = '';
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+</style>
+
---
+++ src/main/frontend/src/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <label>Zaloguj się e-mailem</label>
+        <input type="email" v-model="email">
+        <button @click="enter()">{{ buttonLabel }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["buttonLabel"],
+  data() {
+    return {
+      email: ""
+    };
+  },
+  methods: {
+    enter() {
+      this.$emit("login", this.email);
+    }
+  },
+  mounted() {
+    if (!this.buttonLabel) {
+      this.buttonLabel = "Zaloguj się";
+    }
+  }
+};
+</script>
