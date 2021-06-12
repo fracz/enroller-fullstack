940dc1ea352018e5440a995d54283cadd9973e4e
Kamoool <xkam555@gmail.com>
2021-06-12 15:47:33 +0200

    Registering possible, hashed passwords

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,52 +1,89 @@
 <template>
   <div id="app">
     <h1>
-      <img src="./assets/logo.svg" alt="Enroller" class="logo">
+      <img src="./assets/logo.svg" alt="Enroller" class="logo" />
       System do zapisów na zajęcia
     </h1>
     <div v-if="authenticatedUsername">
-      <h2>Witaj {{ authenticatedUsername }}!
-        <a @click="logout()" class="float-right  button-outline button">Wyloguj</a>
+      <h2>
+        Witaj {{ authenticatedUsername }}!
+        <a @click="logout()" class="float-right button-outline button"
+          >Wyloguj</a
+        >
       </h2>
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+      <button :class="wantToRegister ? 'button-outline' : ''"
+        @click="wantToRegister = false">Loguję się </button>
+      <button :class="wantToRegister ? '' : 'button-outline'"
+        @click="wantToRegister = true">Rejestruję się</button>
+
+
+        <div v-if="errorMessage" class="alert-warning">{{ errorMessage}}</div>
+
+
+      <login-form v-if="!wantToRegister" @login="login($event)"></login-form>
+
+      <login-form v-else @login="register($event)" button-label="Zarejestruj się"></login-form>
     </div>
   </div>
 </template>
 
 <script>
-    import "milligram";
-    import LoginForm from "./LoginForm";
-    import MeetingsPage from "./meetings/MeetingsPage";
+import "milligram";
+import LoginForm from "./LoginForm";
+import MeetingsPage from "./meetings/MeetingsPage";
 
-    export default {
-        components: {LoginForm, MeetingsPage},
-        data() {
-            return {
-                authenticatedUsername: ""
-            };
-        },
-        methods: {
-            login(user) {
-                this.authenticatedUsername = user.login;
-            },
-            logout() {
-                this.authenticatedUsername = '';
-            }
-        }
+export default {
+  components: { LoginForm, MeetingsPage },
+  data() {
+    return {
+      authenticatedUsername: "",
+      wantToRegister: false,
+      errorMessage: ''
     };
+  },
+  methods: {
+    login(user) {
+      this.authenticatedUsername = user.login;
+    },
+    register(user) {
+      this.errorMessage='';
+      this.$http
+        .post("participants", user)
+        .then((response) => {
+          //udało się
+          this.wantToRegister = false;
+          this.errorMessage='';
+        })
+        .catch((response) => {
+          //nie udało się
+          this.errorMessage='Nazwa użytkownika jest zajęta';
+        });
+    },
+    logout() {
+      this.authenticatedUsername = "";
+    },
+  },
+};
 </script>
 
 <style>
-  #app {
-    max-width: 1000px;
-    margin: 0 auto;
-  }
+#app {
+  max-width: 1000px;
+  margin: 0 auto;
+}
 
-  .logo {
-    vertical-align: middle;
-  }
+.logo {
+  vertical-align: middle;
+}
+.alert-warning{
+  border: 3px red dotted;
+  padding: 3px;
+  text-align: center;
+  background: pink;
+  border-radius: 30%;
+}
 </style>
 
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -11,6 +13,9 @@ public class ParticipantService {
 
     DatabaseConnector connector;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    
     public ParticipantService() {
         connector = DatabaseConnector.getInstance();
     }
@@ -24,6 +29,12 @@ public class ParticipantService {
     }
 
     public Participant add(Participant participant) {
+    	String rawPassword = participant.getPassword();    	
+    	
+    	String hashedPassword = this.passwordEncoder.encode(rawPassword);
+    	
+    	participant.setPassword(hashedPassword);
+    	
         Transaction transaction = connector.getSession().beginTransaction();
         connector.getSession().save(participant);
         transaction.commit();
